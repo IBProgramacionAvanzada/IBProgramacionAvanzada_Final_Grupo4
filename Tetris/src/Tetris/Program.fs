750aee3030518142fd53fcb0bdc003af20a2d757
r a c <alogos@gmail.com>
2023-06-21 18:00:49 -0300

    I'm just testing what we have. Probably will wipe out all of this.

--- Tetris/src/Tetris/Program.fs
+++ Tetris/src/Tetris/Program.fs
@@ -1,2 +1,13 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+//printfn "Hello from F#"
+
+open System
+open Library
+
+reactiveKey()
+gravity()
+
+UtilsView.printMappa (mappa.getIstanceWith blocco)
+
+// NO END RN
+System.Threading.Thread.Sleep(-1);
