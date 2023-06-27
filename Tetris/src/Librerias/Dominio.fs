e6d9b396f03b487aa906d26599c43fdc9cf3df02
r a c <alogos@gmail.com>
2023-06-27 20:21:33 -0300

    Empiezo por adoptar el Español para los archivos. Veremos qué sale de esto.

---
+++ Tetris/src/Librerias/Dominio.fs
@@ -0,0 +1,8 @@
+﻿namespace Tetris
+
+open System
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
---
+++ Tetris/src/Tetris/Programa.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+//printfn "Hello from F#"
+
+open Dominio
+
+[<EntryPoint>]
+let main args =
+
+    0
