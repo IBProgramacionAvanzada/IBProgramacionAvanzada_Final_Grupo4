1f96bdd810617a1b1bd846962210d906e04dea77
Camila Soria <37965840+camisoriahomez@users.noreply.github.com>
2023-06-28 16:20:00 -0300

    Update UnitTest1.fs
    Unificamos test en el mismo archivo

--- Tetris/tests/TetrisTests/UnitTest1.fs
+++ Tetris/tests/TetrisTests/UnitTest1.fs
@@ -10,13 +10,21 @@ let Setup () =
     ()
 
 [<Test>]
-let ``Rotacion de bloque`` () = 
+let ``Rotacion de bloque azul`` () = 
     let formaInicial = (Azul, [[O; X; X]; [X; X; O]])
     let formaRotada = rotar (snd formaInicial)
     let formaEsperada = snd (Azul, [[X; O]; [X; X]; [O; X]])
     
     assert (formaRotada = formaEsperada)
-
+    
+[<Test>]
+let ``Rotacion de bloque rojo`` () =
+    let formaInicial = [ [ X; X; X; X ] ]
+    let expected = [ [X]; [X]; [X]; [X] ]                                  
+    let actual = formaInicial |> rotar
+    
+    Assert.AreEqual(expected, actual)
+    
 [<Test>]
 let ``Bloque fuera de rango`` () =
     let bloques = [(9, 5); (10, 5); (5, 21); (-1, 10)]
