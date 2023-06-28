c708629f67de174d32dc854fbd9af82ddf2ed1ba
r a c <alogos@gmail.com>
2023-06-27 23:51:45 -0300

    Planteo inicial de tipos.

--- Tetris/src/Librerias/Dominio.fs
+++ Tetris/src/Librerias/Dominio.fs
@@ -6,3 +6,51 @@ module Say =
     let hello name =
         printfn "Hello %s" name
 
+module Modelo =
+    let aleatorio = new System.Random ()
+
+    type Color = 
+    | Rojo
+    | Magenta
+    | Amarillo
+    | Cian
+    | Azul
+    | Plateado
+    | Verde
+
+    type Marca = 
+    |X
+    |O
+
+    let formas = [
+            Cian, [
+                [X;X]
+                [X;X]
+            ]
+            Rojo, [
+                [X;X;X;X]
+            ]
+            Verde, [
+                [X;X;O]
+                [O;X;X]
+            ]
+            Azul, [
+                [O;X;X]
+                [X;X;O]
+            ]
+            Amarillo, [
+                [X;X;X]
+                [X;O;O]
+            ]
+            Magenta, [
+                [X;X;X]
+                [O;O;X]
+            ]
+            Plateado, [
+                [X;X;X]
+                [O;X;O]
+            ]
+        ]
+    
+    
+
--- Tetris/src/Tetris/Programa.fs
+++ Tetris/src/Tetris/Programa.fs
@@ -1,7 +1,7 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 //printfn "Hello from F#"
 
-open Dominio
+open Tetris
 
 [<EntryPoint>]
 let main args =
