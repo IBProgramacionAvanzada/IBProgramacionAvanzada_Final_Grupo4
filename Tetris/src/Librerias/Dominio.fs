dbd1eda5bf4a8d3232e785f9e39c81c9677b22fb
r a c <alogos@gmail.com>
2023-06-28 01:48:05 -0300

    Avance en la definición del modelo.

--- Tetris/src/Librerias/Dominio.fs
+++ Tetris/src/Librerias/Dominio.fs
@@ -9,18 +9,22 @@ module Say =
 module Modelo =
     let aleatorio = new System.Random ()
 
+    let ancho, alto = 10, 20
+    let posInicial = (ancho / 2 - 1, 0)
+    let puntajePorLinea = 100
+
     type Color = 
-    | Rojo
-    | Magenta
-    | Amarillo
-    | Cian
-    | Azul
-    | Plateado
-    | Verde
+        | Rojo
+        | Magenta
+        | Amarillo
+        | Cian
+        | Azul
+        | Plateado
+        | Verde
 
     type Marca = 
-    |X
-    |O
+        |X
+        |O
 
     let formas = [
             Cian, [
@@ -52,5 +56,33 @@ module Modelo =
             ]
         ]
     
-    
+    type Comando =
+        | Derecha
+        | Izquierda
+        | Rotar
 
+    type Evento =
+        | Movido 
+        | Rotado 
+        | Bloqueado 
+        | Lanzado 
+        | Linea 
+        | IncrementoNivel 
+        | FinDelJuego
+
+    type Tablero = {
+        puntaje: int
+        nivel: int
+        juegoFinalizado: bool
+
+        tiempoUltimoComando: float
+        tiempoUltimaCaida: float
+        tiempoUltimaLinea: float
+        lineasParaQuitar: (Color * int * int) list option
+
+        bloquesEstaticos: (Color * int * int) list
+        pos: int * int
+        forma: (Color * Marca list list) option
+        siguiente: Color * Marca list list
+        eventos: Evento list
+    } 
