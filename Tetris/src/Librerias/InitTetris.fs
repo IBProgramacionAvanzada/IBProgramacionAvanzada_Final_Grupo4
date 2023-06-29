e13efb51ff4b9add15afb3105fbce7ed20291643
camisoriahz <camilasoriahz@gmail.com>
2023-06-28 22:26:39 -0300

    Se eliminan constantes de tiempo no usadas y campos de Tablero

--- Tetris/src/Librerias/InitTetris.fs
+++ Tetris/src/Librerias/InitTetris.fs
@@ -7,9 +7,7 @@ module Inicializar =
     let posicionInic = (ancho / 2 - 1, 0)
     let puntuacionPorLinea = 100
 
-    let tiempoEntreComandos = 200.
-    let tiempoEntreLineas = 1000.
-    let tiempoEntreCaidas = 1000.
+    let tiempoEntreCaidas = 1000. // en milisegundos
     let tiempoMinCaida = 100.
 
     let random = new System.Random ()
@@ -19,9 +17,7 @@ module Inicializar =
         puntuacion = 0
         juegoTerminado = false
 
-        comandoAnteriorTiempo = 0.
         caidaAnteriorTiempo = 0.
-        lineaAnteriorTiempo = 0.
         lineasAEliminar = None
         
         bloquesEstaticos = []
